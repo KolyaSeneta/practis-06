06412bf8e306c1e7e7072275d6c97c67d2836c25
KolyaSeneta <114915582+KolyaSeneta@users.noreply.github.com>
2023-02-06 15:26:48 +0200

    1-st

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,49 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { logDOM } from "@testing-library/react";
+import { Component } from "react";
+import SelectBasicExample from "./Select.jsx"
+
+
+class App extends Component {
+  state= {
+    
+    img: [],
+    value:"1" 
+  }
+
+  changeValue = (e) => {
+    this.setState({
+      value: e.target.value,
+    })
+    console.log(e.target.value)
+    
+  }
+   fetchApi(){
+     fetch(`https://dog.ceo/api/breeds/image/random/${this.state.value}`).then(r => r.json())
+       .then(r => this.setState({
+       img: r.message
+     }))
+     
+     }
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.value !== this.state.value) {
+     this.fetchApi()
+    }
+  
+  }
+
+
+
+  
+  
+  render() {
+    return (
+    < div >
+    <SelectBasicExample changeValue = {this.changeValue} /> 
+    </div >
+  )
+}
+  
+
+}
+
+export default App
